052ddef794de85f1d094e3afe05f096258fd400a
Katsuey2 <162149209+Katsuey2@users.noreply.github.com>
2024-04-21 00:00:55 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let Yes= document.getElementById('Ty')
+
+
+Yes.onclick = function(){
+  thankyou = document.getElementById('para2').textContent 
+  = `Hey Anne,today is the day where you go somewhere far away and live a new life.But before that happens I want to thank you for bringing happiness and joy to my life.I swear ever since I met you I never felt so loved in my life.Being your boyfriend felt like I was the luckiest man  in the world.Its just that you're so pretty and smart and kind you're just perfect.but as you leave,I want you to promise me that your 
+  going to live the best life you can without me.I'm not sure if we are going to even see each other again but I'll try my best.Thank you for everything, Anne Catherine. Farewell, and may the road ahead be as Beautifull as you are.        
+  Love,Xian`
+  
+}
+
+
+
---
+++ other.js
@@ -0,0 +1,6 @@
+let No = document.getElementById('Nobtn')
+
+No.onclick = function() {
+  response = document.getElementById('para').textContent = "ERROR:YOu MUST BE ANNE CATHERINE!!!!!"
+  
+}
